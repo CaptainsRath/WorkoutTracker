fed7b6b6bb4cb30873173ee51f9041c560a85bff
Sergiu Pod <sergiupod@gmail.com>
2025-07-16 10:54:45 -0500

    updated folder structure

---
+++ fitness-pal/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <main className='w-full h-full flex justify-center bg-emerald-600'>
+            <h1 className='font-bold'>HOME ROUTE WORKS</h1>
+        </main>
+    )
+}
