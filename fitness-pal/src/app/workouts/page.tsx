34bea1201399bca86f3ead748132d51ec644990e
Lucas Eby <lucaseby@outlook.com>
2025-08-01 15:52:43 -0400

    Added database connection and displayed dates in reference to the current server's time on the Workouts page's cards

--- fitness-pal/src/app/workouts/page.tsx
+++ fitness-pal/src/app/workouts/page.tsx
@@ -1,15 +1,65 @@
+import { env } from "@/src/env";
+import { createConnection } from "mysql2/promise";
 import GenericCard from '@/src/components/genericCard'
+
+// DB data used for the cards
+interface WorkoutTemplateData {
+    workoutId: number;
+    userId: number;
+    lastDate: Date;
+    name: string;
+}
+
+function getTimeSince(date: Date): string {
+    const now = new Date();
+    const secondsSince = (now.getTime() - date.getTime()) / 1000;
+    
+    // seconds is the amount of 1 full unit of specified time in seconds
+    const timeVars = [
+        {unit: "year",   seconds: 60 * 60 * 24 * 365},
+        {unit: "week",   seconds: 60 * 60 * 24 * 7},
+        {unit: "day",    seconds: 60 * 60 * 24},
+        {unit: "hour",   seconds: 60 * 60},
+        {unit: "minute", seconds: 60},
+        {unit: "second", seconds: 1},
+    ]
+
+    // Return the time since in the greatest unit of time
+    for (const currVar of timeVars) {
+        const amount = Math.floor(secondsSince / currVar.seconds);
+        if (amount > 0) {
+            var timeStr = ""
+            if (amount === 1) {
+                timeStr = currVar.unit;
+            } else {
+                timeStr = `${currVar.unit}s`;
+            }
+            return `About ${amount} ${timeStr} ago`;
+        }
+    }
+    return "Now"
+}
 
 // Shows all workouts
 export default async function Workouts() {
+    const conn = await createConnection(env.DATABASE_URL);
+    // Query for the ID and name of all exercises
+    const userId = 1; // TODO: replace with a paramter passed into the file
+    const [workoutTemplates, _] = await conn.execute<WorkoutTemplateData[]>(
+        'SELECT workoutId, lastDate, name FROM WorkoutTemplates WHERE userId = ? ORDER BY lastDate ASC',
+        [userId]
+    );
+    await conn.end();
+    
     return (
         <main className='w-full h-fit flex-wrap bg-blue-700 rounded'>
-            <h1 className='font-bold w-full text-center'>WORKOUTS ROUTE</h1>
+            <h1 className='font-bold w-full text-center'>WORKOUTS</h1>
             <section className='grid grid-flow-row gap-5 grid-cols-3 mx-5 mb-5 [&>*]:bg-blue-500'>
-                {Array.from({ length: 20 }).map((_, idx) => {
+                {workoutTemplates.map((workoutTemplates) => {
                     return (
-                        <GenericCard href={`/workouts/${idx}`} key={`workout-id-${idx}`}>
-                            Workout {idx}
+                        <GenericCard href={`/workouts/${workoutTemplates.workoutId}`} key={workoutTemplates.workoutId}>
+                            <div className="font-bold">{workoutTemplates.name}</div>
+                            <div className="text-sm text-gray-100">{getTimeSince(workoutTemplates.lastDate)}</div>
                         </GenericCard>
                     )
                 })}
